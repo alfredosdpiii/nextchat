4a870b3e8463382855e36109f3e1381478056bdc
alfredosdpiii <alfredo.sdpiii@gmail.com>
2024-09-14 20:27:44 +0800

    next auth update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,8 @@
-export default function About() {
+export default function AboutRoute() {
   return (
     <main>
-      <h1 className="text-4xl font-bold">About us</h1>
-      <div className="mt-5">About us</div>
+      <h1 className="text-4xl font-bold">About Us</h1>
+      <div className="mt-5">About Us And Our Awesome Application</div>
     </main>
   );
 }
