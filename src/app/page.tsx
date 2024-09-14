4afe4112735ba47c88f99b196d4fd63150bfb8e9
alfredosdpiii <alfredo.sdpiii@gmail.com>
2024-09-14 20:36:07 +0800

    update render logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ import Chat from "@/app/components/Chat";
 
 export default async function Home() {
   const session = await getServerSession();
+  console.log(session.user);
 
   return (
     <main className="p-5">
       <h1 className="text-4xl font-bold">Welcome To GPT Chat</h1>
-      {!session?.user?.email && <div>You need to log in to use this chat.</div>}
-      {session?.user?.email && (
+      {!session?.user && <div>You need to log in to use this chat.</div>}
+      {session?.user && (
         <>
           <Separator className="my-5" />
           <Chat />
